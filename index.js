bfb14ef1ee36fc8493107ceb73a8a75eaf9c973e
EXtremeExploit <gdextremeexploit@gmail.com>
2018-07-22 23:30:49 -0300

    Optimized code, so now every bot has the same code

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 require('dotenv').config();
 
-// Link shorter
-var isgd = require('isgd');
+var subreddit = require('./subreddit');
+new subreddit('TakimotoHifumi', {
+    twitter: {
+        access_token: process.env.TakimotoHifumi_twitter_accesToken,
+        access_token_secret: process.env.TakimotoHifumi_twitter_accesTokenSecret,
+        consumer_key: process.env.TakimotoHifumi_twitter_consumerKey,
+        consumer_secret: process.env.TakimotoHifumi_twitter_consumerSecret,
+    },
+    reddit: {
+        username: process.env.reddit_username,
+        password: process.env.reddit_password,
+        userAgent: 'r/TakimotoHifumi Twitter Bot',
+        clientId: process.env.TakimotoHifumi_reddit_clientID,
+        clientSecret: process.env.TakimotoHifumi_reddit_clientSecret
+    }
+});
 
-var TakimotoHifumi = require('./TakimotoHifumi');
-new TakimotoHifumi();
-var SuzukazeAoba = require('./SuzukazeAoba');
-new SuzukazeAoba();
-
+new subreddit('SuzukazeAoba', {
+    twitter: {
+        access_token: process.env.SuzukazeAoba_twitter_accesToken,
+        access_token_secret: process.env.SuzukazeAoba_twitter_accesTokenSecret,
+        consumer_key: process.env.SuzukazeAoba_twitter_consumerKey,
+        consumer_secret: process.env.SuzukazeAoba_twitter_consumerSecret,
+    },
+    reddit: {
+        username: process.env.reddit_username,
+        password: process.env.reddit_password,
+        userAgent: 'r/SuzukazeAoba Twitter Bot',
+        clientId: process.env.SuzukazeAoba_reddit_clientID,
+        clientSecret: process.env.SuzukazeAoba_reddit_clientSecret
+    }
+});
 console.log('Started');
---
+++ subreddit.js
@@ -0,0 +1,39 @@
+class Subreddit {
+    /**
+     * Subreddit
+     */
+    constructor(subreddit, credentials) {
+        this.subreddit = subreddit;
+        this.credentials = credentials;
+
+        // Link shorter
+        var isgd = require('isgd');
+
+        // Twitter setup
+        var _twit = require('twit');
+        const twit = new _twit(this.credentials.twitter);
+
+        // Reddit setup
+        var _reddit = require('snoowrap');
+        var snoostorm = require('snoostorm');
+        var r = new _reddit(this.credentials.reddit);
+        const reddit = new snoostorm(r);
+
+        // The bot itself
+        reddit.SubmissionStream({
+            subreddit: this.subreddit
+        }).on('submission', sub => {
+            isgd.shorten('https://www.reddit.com/' + sub.permalink, (link) => {
+                twit.post('statuses/update', {
+                    status: '(' + link + ') ' + sub.title
+                }).then((tw) => {
+                    console.log('[r/' + this.subreddit + '] Posted:' + tw.data.text);
+                }).catch(err => console.log(err));
+            });
+
+        });
+        console.log('Loaded r/' + this.subreddit);
+    }
+}
+
+module.exports = Subreddit;
