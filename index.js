39a07d874e614ce119b00e67b7a95083324f23fe
EXtremeExploit <elalfagamermc@gmail.com>
2019-09-08 12:11:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var subreddit = require('./subreddit');
 //Reset the bot every 23 hours(82800000ms)
 setInterval(function() {
-	process.exit(1);
+	process.exit();
 }, 82800000);
 new subreddit('TakimotoHifumi', {
     twitter: {
